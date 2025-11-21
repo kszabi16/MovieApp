774fcb04e3a7b2b69d5684c88271e6fd066f1f39
kszabi16 <kiss.szabi16@gmail.com>
2025-11-21 12:10:05 +0100

    genrecontroller

---
+++ MovieApp/Controllers/GenreController.cs
@@ -0,0 +1,35 @@
+﻿
+using MovieApp.DataContext.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.DataContext.Context;
+
+namespace MovieApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly MovieAppDbContext _context;
+
+        public GenreController(MovieAppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get all genres
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GenreDto>>> GetGenres()
+        {
+            var genres = await _context.Genres
+                .Select(g => new GenreDto
+                {
+                    Id = g.Id,
+                    Name = g.Name
+                })
+                .ToListAsync();
+
+            return Ok(genres);
+        }
+    }
+}
